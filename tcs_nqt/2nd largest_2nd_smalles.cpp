86d0097b3d50cdde0b0fc410b750df8a4e7e2f77
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-11-05 12:51:06 +0530

    Create 2nd largest_2nd_smalles.cpp

---
+++ tcs_nqt/2nd largest_2nd_smalles.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++) cin >> a[i];
+    sort(a.begin(), a.end());
+    auto it = unique(a.begin(), a.end());
+    a.resize(distance(a.begin(), it));
+    if(a.size()<2) cout << -1 <<endl;
+    else {
+        cout << a[1] << " " <<  a[a.size()-2] <<endl;
+    }
+}
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++) cin >> arr[i];
+    int a = INT_MIN, b = INT_MIN;
+    int c = INT_MAX, d = INT_MAX;
+    for(auto it:arr) {
+        if(it>a) {
+            b = a;
+            a = it;
+        }
+        else if(it>b && it != a) b = it;
+        if(it<c) {
+            d = c;
+            c = it;
+        }
+        else if(it<d && it != c) d = it;
+    }
+    cout << d << " " << b <<endl;
+}
