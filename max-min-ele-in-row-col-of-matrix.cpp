612ff4bed048c6d2b59362ff2328c39db806e19f
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-12-21 21:21:14 +0530

    Update max-min-ele-in-row-col-of-matrix.cpp

--- max-min-ele-in-row-col-of-matrix.cpp
+++ max-min-ele-in-row-col-of-matrix.cpp
@@ -6,11 +6,11 @@ for(int i=0;i<n;i++) {
       for(int j=0;j<m;j++) mn = max(mn, a[i][j]);
 }
 
-for(int j=0;i<m;j++) {
+for(int j=0;j<m;j++) {
       for(int i=0;i<n;i++) mx = max(mx, a[i][j]);
 }
 
-for(int j=0;i<m;j++) {
+for(int j=0;j<m;j++) {
       for(int i=0;i<n;i++) mn = max(mn, a[i][j]);
 }
 
