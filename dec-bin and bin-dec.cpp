bdac276f1fb4c87e246ef4c2f91b4e442fa2a2a9
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-12-22 12:30:38 +0530

    Update dec-bin and bin-dec.cpp

--- dec-bin and bin-dec.cpp
+++ dec-bin and bin-dec.cpp
@@ -3,11 +3,7 @@ long int dectoBin(int n) {
     else return n%2 + 10*octal(n/2);
 }
 
-int bintodec(string &s) {
-    int dec = 0, p = 0;
-    for(int i=s.length()-1;i>=0;i--) {
-        if(s[i] == '1') dec += pow(2, p);
-        p++;
-    }
-    return dec;
+long int binToDec(int n) {
+    if(n == 0) return 0;
+    else return n%10 + 2*binToDec(n/10);
 }
