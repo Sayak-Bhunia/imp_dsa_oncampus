526242a8dcfaad543a19454bbbf76d028f19eacb
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-12-22 12:31:55 +0530

    Update dec-oct and oct-dec.cpp

--- dec-oct and oct-dec.cpp
+++ dec-oct and oct-dec.cpp
@@ -3,11 +3,7 @@ long int dectoOctal(int n) {
     else return n%8 + 10*octal(n/8);
 }
 
-int octalToDec(string n) {
-    int dec = 0, p = 0;
-    for(int i=n.length()-1;i>=0;i--) {
-        if(n[i]>='0' && n[i]<='7') dec += (n[i] - '0')*pow(8, p);
-        p++;
-    }
-    return dec;
+long int octToDec(int n) {
+    if(n == 0) return 0;
+    else return n%10 + 8*binToDec(n/10);
 }
