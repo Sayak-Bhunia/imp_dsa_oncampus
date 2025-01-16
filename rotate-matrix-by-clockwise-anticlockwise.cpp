8984f1c9df4e08e4bac684390bf9c2157b13b20e
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2025-01-16 18:39:27 +0530

    Update rotate-matrix-by-clockwise-anticlockwise.cpp

--- rotate-matrix-by-clockwise-anticlockwise.cpp
+++ rotate-matrix-by-clockwise-anticlockwise.cpp
@@ -1,12 +1,21 @@
 void rotateClockwise(vector<vector<int>>& a) {
     int n = a.size();
-    for(int i=0;i<n;i++) {
-        for(int j=i+1;j<n;j++) swap(a[i][j], a[j][i]);
+    vector<vector<int>> temp(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            temp[j][n - i - 1] = a[i][j];
+        }
     }
-    for(int i=0;i<n;i++) reverse(a[i].begin(), a[i].end());
+    a = temp; 
 }
 
-for (int i=0;i<n;i++) {
-    for (int j=0;j<m;j++) temp[m-j-1][i] = a[i][j];
+void rotateAnticlockwise(vector<vector<int>>& a) {
+    int n = a.size();
+    vector<vector<int>> temp(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            temp[n - j - 1][i] = a[i][j];
+        }
+    }
+    a = temp; 
 }
-a = temp;
