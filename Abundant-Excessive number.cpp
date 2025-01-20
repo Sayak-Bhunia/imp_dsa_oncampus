89a51802790e0c0a09b0e6c88b24bac59535df7d
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2025-01-20 18:13:26 +0530

    Create Abundant-Excessive number.cpp

---
+++ Abundant-Excessive number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int m = 0;
+    for(int i=1;i<=n/2;i++) {
+        if(n%i == 0) m += i;
+    }
+    if(m > n) cout << "YES" <<endl;
+    else cout << "NO" <<endl;
+}
