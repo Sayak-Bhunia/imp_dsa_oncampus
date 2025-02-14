731e66536a21f830e434c40359fe553090bef64c
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2025-02-14 10:00:50 +0530

    Update n-people-r-seats-permutation-pigeonhole-principle.cpp

--- n-people-r-seats-permutation-pigeonhole-principle.cpp
+++ n-people-r-seats-permutation-pigeonhole-principle.cpp
@@ -1,12 +1,17 @@
-unsigned long long fact(int n) {
-    unsigned long long p = 1;
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+#define lint long long int
+
+lint f(int n) {
+    lint p = 1;
     for(int i=1;i<=n;i++) p *= i;
     return p;
 }
 
-unsigned long long helper(int n, int r) {
+lint helper(int n, int r) {
     if(r>n) return 0;
-    return fact(n)/fact(n-r);
+    return f(n)/f(n-r);
 }
 
 int main() {
