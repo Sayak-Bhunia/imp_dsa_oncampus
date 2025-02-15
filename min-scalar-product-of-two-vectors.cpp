b61d6028b8ce781862fd7c7a1fe0ab70926f6001
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2025-02-15 10:14:38 +0530

    Create min-scalar-product-of-two-vectors.cpp

---
+++ min-scalar-product-of-two-vectors.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {   
+    int n = 5;
+    vector<int> a(n), b(n);
+    sortasc(a);
+    sortdsc(b);
+    for(int i=0;i<n;i++) mn += 1ll*a[i]*b[i];
+    cout << mn <<endl;
+}
