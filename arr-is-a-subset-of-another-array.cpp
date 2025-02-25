453fa5f35f57ef882122f4701cd9677f8cc144da
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2025-02-25 09:14:34 +0530

    array subset subarray subsequence.cpp

--- arr-is-a-subset-of-another-array.cpp
+++ arr-is-a-subset-of-another-array.cpp
@@ -2,6 +2,7 @@
 #define lint long long 
 using namespace std;
 
+// isSubset
 int main() {   
     int n,m;
     cin >> n >> m;
@@ -18,4 +19,29 @@ int main() {
     }
     if(f) cout << "NO" <<endl;
     else cout << "YES" <<endl;
+}
+
+bool isSubsequence(vector<int>& a, vector<int>& b) {
+    int n = a.size(),  m = b.size();
+    int i = 0, j = 0;
+    while(i<n && j<m) {
+        if(a[i] == b[j]) j++;
+        i++;
+    }
+    return (j == m);  
+}
+
+bool isSubarray(vector<int>& A, vector<int>& B) {
+    int n = A.size(), m = B.size();
+    for(int i=0;i<=n-m;i++) {
+        bool match = true;
+        for(int j=0;j<m;j++) {
+            if(A[i+j] != B[j]) {
+                match = false;
+                break;
+            }
+        }
+        if (match) return true;
+    }
+    return false; 
 }
