d47b8cca2d2831551fc30eb0483228523b61cb83
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2025-02-28 19:15:58 +0530

    Update rotate-matrix-by-k-times.cpp

--- rotate-matrix-by-k-times.cpp
+++ rotate-matrix-by-k-times.cpp
@@ -1,58 +1,40 @@
-#include <bits/stdc++.h>
-#define lint long long 
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-void rotateMatrix(vector<vector<int>> &a) {
-    int n = a.size();
-    int m = a[0].size();
-    int row = 0, col = 0;
-    int prev, curr;
-    while(row<n && col<m) {
-        if(row + 1 == n || col + 1 == m) break;
-        prev = a[row+1][col];
-        for(int i=col;i<m;i++) {
-            curr = a[row][i];
-            a[row][i] = prev;
-            prev = curr;
-        }
-        row++;
-        for(int i=row;i<n;i++) {
-            curr = a[i][m-1];
-            a[i][m-1] = prev;
-            prev = curr;
-        }
-        m--;
-        if(row<n) {
-            for(int i=m-1;i>=col;i--) {
-                curr = a[n-1][i];
-                a[n-1][i] = prev;
-                prev = curr;
-            }
-        }
-        n--;
-        if(col<m) {
-            for(int i=n-1;i>=row;i--) {
-                curr = a[i][col];
-                a[i][col] = prev;
-                prev = curr;
-            }
-        }
-        col++;
+void rotateMat(vector<vector<int>> &a, int k) {
+    int n = a.size(), m = a[0].size();
+    int top = k, left = k, bottom = n - k - 1, right = m - k - 1;
+    if(top>=bottom || left>=right) return;
+    vector<int> res;
+    for(int i=left;i<=right;i++) res.push_back(a[top][i]);
+    for(int i=top+1;i<=bottom;i++) res.push_back(a[i][right]);
+    for(int i=right-1;i>=left;i--) res.push_back(a[bottom][i]);
+    for(int i=bottom-1;i>=top;i--) res.push_back(a[i][left]);
+    rotate(res.begin(), res.begin() + res.size() - 1, res.end());
+    int idx = 0;
+    for(int i=left;i<=right;i++) a[top][i] = res[idx++];
+    for(int i=top+1;i<=bottom;i++) a[i][right] = res[idx++];
+    for(int i=right-1;i>=left;i--) a[bottom][i] = res[idx++];
+    for(int i=bottom-1;i>=top;i--) a[i][left] =res[idx++];
+}
+
+void helper(vector<vector<int>> &a, int n, int m, int k) {
+    int layers = min(n, m)/2;
+    for(int i=0;i<k;i++) {
+        for(int j=0;j<layers;j++) rotateMat(a, j);
     }
 }
 
-void rotateMatrixKTimes(vector<vector<int>>& a, int k) {    
-    for(int i=0;i<k;i++) rotateMatrix(a);
-}
-
-int main() {   
+int main() {
     int n,m,k;
     cin >> n >> m >> k;
     vector<vector<int>> a(n, vector<int>(m));
     for(int i=0;i<n;i++) {
         for(int j=0;j<m;j++) cin >> a[i][j];
     }
-    rotateMatrixKTimes(a, k);
+    helper(a, n, m, k);
     for(int i=0;i<n;i++) {
         for(int j=0;j<m;j++) cout << a[i][j] << " ";
         cout <<endl;
