711b2dcdc26d935b0e10f04e4c6a4e75f1395d54
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2025-03-01 17:56:54 +0530

    Update replace-substring-in-a-string.cpp

--- replace-substring-in-a-string.cpp
+++ replace-substring-in-a-string.cpp
@@ -1,8 +1,30 @@
-string replaceSubstring(string s, const string &t, const string &r) {
-    size_t pos = s.find(t); 
-    while(pos != string::npos) {
-        s.replace(pos, t.length(), r); 
-        pos = s.find(t, pos + r.length()); 
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+// a = string to be replaced
+// b = string to be replaced with
+
+string helper(string &s, string &a, string &b) {
+    string res;
+    int n = s.length(), m = a.length();
+    int i = 0;
+    while(i<n) {
+        if(i<=n-m && s.substr(i, m) == a) {
+            res += b;
+            i += m;
+        }
+        else {
+            res += s[i];
+            i++;
+        }
     }
-    return s;
+    return res;
+}
+
+int main() {
+    string s,a,b;
+    cin >> s >> a >> b;
+    cout << helper(s, a, b) <<endl;
 }
