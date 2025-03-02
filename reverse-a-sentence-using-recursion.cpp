3741b01f93a94a93f162d3c4284ce8f0d2046eb5
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2025-03-02 18:32:58 +0530

    Update reverse-a-sentence-using-recursion.cpp

--- reverse-a-sentence-using-recursion.cpp
+++ reverse-a-sentence-using-recursion.cpp
@@ -1,5 +1,15 @@
-void reverse(string s) {
-    if(s.empty()) return; 
-    cout << s.back();
-    reverse(s.substr(0, s.size() - 1));
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void helper(string &s, int n) {
+    if(n<0) return;
+    cout << s[n];
+    helper(s, n-1);
+}
+
+int main() {
+    string s;
+    getline(cin, s);
+    helper(s, s.length() - 1);
 }
