98f24aca40e9ecccfdba1ff08f5bbbe10f127d27
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2025-06-18 20:33:58 +0530

    Create stairs combination.cpp

---
+++ accenture/stairs combination.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define lint long long
+using namespace std;
+
+int helper(int n) {
+    /* dp approach =>
+    vector<int> dp(n+1, 0);
+    dp[0] = 1;
+    dp[1] = 1;
+    for(int i=2;i<=n;i++) dp[i] = dp[i-1] + dp[i-2];
+    */ 
+
+    // space optimized approach =>
+    int prev1 = 1, prev2 = 1, curr;
+    for(int i=2;i<=n;i++) {
+        curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    return curr;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << helper(n) <<endl;
+}
