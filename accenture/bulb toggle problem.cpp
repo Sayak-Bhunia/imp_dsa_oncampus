b787873f9307ba2c15495f09283befd44260aceb
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2025-06-21 12:13:31 +0530

    Create bulb toggle problem.cpp

---
+++ accenture/bulb toggle problem.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define lint long long
+using namespace std;
+
+int helper(int n) {
+    cout << (int)sqrt(n) <<endl;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    helper(n);
+}
