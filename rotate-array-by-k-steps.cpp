7f5aeb5a76851deb941674ce8ef24652a0d2e32f
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2025-08-04 14:14:10 +0530

    Update rotate-array-by-k-steps.cpp

--- rotate-array-by-k-steps.cpp
+++ rotate-array-by-k-steps.cpp
@@ -1,15 +1,9 @@
-#include <bits/stdc++.h>
-#define lint long long 
-using namespace std;
-
-int main() {   
-    int n,k;
-    cin >> n >> k;
-    vector<int> a(n);
-    for(int i=0;i<n;i++) cin >> a[i];
-    int x = k%n;
-    reverse(a.begin(), a.begin() + x);
-    reverse(a.begin() + x, a.end());
-    reverse(a.begin(), a.end());
-    for(auto it:a) cout << it << " ";
-}
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+       int a =k%nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+a);
+        reverse(nums.begin()+a,nums.end());
+    }
+};
