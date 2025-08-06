a04fe319feec0f12693e93bc3b6c99034cf413e6
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2025-08-06 17:09:52 +0530

    Create valid parenthesis.cpp

---
+++ sap labs/valid parenthesis.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.length() == 0) return true;
+        stack<int> st;
+        for(int i=0;i<s.length();i++) {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') st.push(s[i]);
+            else if(st.empty() && (s[i] == ')' || s[i] == '}' || s[i] == ']')) return false;
+            else {
+                if(st.top() == '(' && s[i] == ')') st.pop();
+                else if(st.top() == '{' && s[i] == '}') st.pop();
+                else if(st.top() == '[' && s[i] == ']') st.pop();
+                else return false;
+            }
+        }
+        if(!st.empty()) return false;
+        else return true;
+    }
+};
