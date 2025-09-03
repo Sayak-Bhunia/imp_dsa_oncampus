a7b765505d5136372d2e0d4cd66980f4fa4cc05d
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2025-09-03 17:42:36 +0530

    Update Kaprekar number.cpp

--- Kaprekar number.cpp
+++ Kaprekar number.cpp
@@ -1,12 +1,4 @@
-import java.util.*;
-
-class A {
-    public static boolean helper(int n) {
-        if(n == 1) return true;
-        int m = n*n;
-        int c = (int) Math.log10(m) + 1, d = 1;
-        for(int i=1;i<c;i++) {
-            d *= 10;#include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 bool isKaprekar(long long n) {
@@ -37,17 +29,4 @@ int main() {
         if (isKaprekar(i)) cout << i << " ";
     }
     return 0;
-}
-
-            int a = m/d, b = m%d;
-            if(a != 0 && b != 0 && a + b == n) return true;
-        }
-        return false;
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        if(helper(n)) System.out.println("Kaprekar");
-        else System.out.println("not Kaprekar");
-    }
 }
